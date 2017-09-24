8c3f313b91924ee8b139b3a65ed120823d8523bf
Валерий Иванов <biostrar520le@gmail.com>
2017-09-24 16:57:27 +0300

    add modal error

--- src/app/content/home/center-main/services/services.component.ts
+++ src/app/content/home/center-main/services/services.component.ts
@@ -10,12 +10,14 @@ import { Service, ServiceService, Error } from '../../../../shared/index';
 export class ServicesComponent implements OnInit, AfterViewInit {
     services: Array<Service>;
     errorMessage: Error;
+    errorMessageShow: string = 'show-modal';
+
     @ViewChild('modal') el: ElementRef;
     constructor(private service: ServiceService, private rd: Renderer2) { }
 
     ngOnInit() {
-        // this.getServices(); // для проверки отрабатываения модалки ошибки
-        this.getToken()
+        this.getServices(); // для проверки отрабатываения модалки ошибки
+        // this.getToken()
 
     }
     ngAfterViewInit() {
@@ -29,9 +31,18 @@ export class ServicesComponent implements OnInit, AfterViewInit {
     }
     private getToken() {
         this.service.getToken().subscribe(x => x.getAPI().subscribe(
-            data => this.services = data,
-            error => this.errorMessage = error
+            data => {this.services = data; console.log('data:', data)},
+            error => {this.errorMessage = error; console.log('data:', error)}
         ));
     }
-
+    private CloseModal() {
+        this.errorMessageShow = '';
+    }
+    private ShowModal() {
+        this.errorMessageShow = 'show-modal';
+    }
+    private Login() {
+        this.errorMessage = undefined;
+        this.getToken();
+    }
 }
