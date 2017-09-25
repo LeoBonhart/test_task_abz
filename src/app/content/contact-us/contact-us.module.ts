5d5a3d098e66e3115d65a477ffb835ba1ae1c5d2
Валерий Иванов <biostrar520le@gmail.com>
2017-09-25 22:04:20 +0300

    ADD: module header and 2 component

--- src/app/content/contact-us/contact-us.module.ts
+++ src/app/content/contact-us/contact-us.module.ts
@@ -2,21 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ContactUsComponent }  from './contact-us.component';
-import { HeaderComponent } from './header/index';
+import { HeaderModule } from './header/index';
 import { BodyComponent } from './body/index';
 import { FooterModule } from './footer/index';
 import { ContactUsRoutingModule } from './contact-us-routing.module';
 
 @NgModule({
-    imports: [CommonModule, ContactUsRoutingModule, FooterModule],
+    imports: [CommonModule, ContactUsRoutingModule, FooterModule, HeaderModule],
     declarations: [
         ContactUsComponent,
-        HeaderComponent,
         BodyComponent
     ],
     exports: [
         ContactUsComponent,
-        HeaderComponent,
         BodyComponent
     ],
     providers: []
